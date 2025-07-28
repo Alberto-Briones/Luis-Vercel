e88c6c466bc00130690cdbc1f0ea461df1edb6cf
Luis Escorcia <19030394@itesa.edu.mx>
2025-07-27 18:08:21 -0600

    Agregacion codigo javascript evento redes sociales

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
   const btn = document.getElementById('button');
   const form = document.getElementById('form');
+  const icons = document.querySelectorAll('social-icons');
 
   form.addEventListener('submit', function (event) {
     event.preventDefault();
     gtag('event', 'formulario_enviado', {
       'event_category': 'Formulario',
-      'event_label': 'Formulario de contacto',
+      'event_label': 'Formulario de contacto', 
       'value': 1
     });
-
+    
     btn.textContent = 'Cargando...';
 
-    const serviceID = 'default_service';      // Reemplaza si usas otro servicio
-    const templateID = 'template_m1s7vqb';    // Reemplaza con tu ID de plantilla
+    const serviceID = 'default_service';      // ID del servicio de envío de correo electrónico
+    const templateID = 'template_m1s7vqb';    
 
     emailjs.sendForm(serviceID, templateID, this)
       .then(() => {
@@ -25,4 +26,17 @@ document.addEventListener('DOMContentLoaded', function () {
         alert('Error al enviar: ' + JSON.stringify(err));
       });
   });
+
+  icons.forEach(icon => {
+    icon.addEventListener('click', function () {
+      const network = this.getAttribute('data-network');
+
+      gtag('event', 'click_red_social', {
+        'event_category': 'Redes Sociales',
+        'event_label': network,
+        'value': 1
+      });
+    });
+  });
+
 });
